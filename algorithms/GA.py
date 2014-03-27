e424beb079e3533bef1f27304ea226d03a61cc56
Anto Čabraja <cabraja.anto@gmail.com>
2014-03-27 18:33:25 +0100

    Genom generator

--- algorithms/GA.py
+++ algorithms/GA.py
@@ -4,17 +4,36 @@ Created on Thu Mar 27 13:42:09 2014
 
 @author: anto
 """
-import fitness
-
+#import fitness
+import random
 class Genome:
     def __init__(self):
         self.genome = dict()
         
-    def mutation(self):
-        pass
+    def __str__(self):
+        return self.genome
     
-    def fit(self):
-        pass
+    def mutation(self):
+        numKlas = len(self.genome)
+        r = random.sample(range(0,numKlas),2)
+        ## mutation 
+    
+    def generateGenome(self,numKlasters, dataNum):
+        l = [i for i in range(dataNum)]
+        random.shuffle(l)        
+        randList =sorted( random.sample(range(dataNum-1),numKlasters-1))
+        randList.append(dataNum)
+        befor = 0
+        k = 0
+        for i in randList:
+            self.genome[k] = l[befor:i]
+            befor = i
+            k+=1
+        
+                        
+            
+    
+    
 
 class Population:
     def __init__(self):
@@ -27,5 +46,4 @@ class Population:
 class Algorithm:
     def __init__(self):
         pass
-    
     
