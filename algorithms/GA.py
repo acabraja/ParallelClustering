5cea8ba1380f2a467f4e3d5e567cfe1ee024c040
Anto Čabraja <cabraja.anto@gmail.com>
2014-04-10 00:37:03 +0200

    sitne promjene

--- algorithms/GA.py
+++ algorithms/GA.py
@@ -14,14 +14,30 @@ class Genome:
         return self.genome
     
     def mutation(self):
-        numKlas = len(self.genome)
-        r = random.sample(range(0,numKlas),2)
-        ## mutation 
+        numClas = len(self.genome)
+        for i in range(10):        
+            r = random.sample(range(0,numClas),2)
+            pom = self.genome[r[0]][1]
+            self.genome[r[0]][1] = self.genome[r[1]][1]
+            self.genome[r[1]][1] = pom 
     
-    def generateGenome(self,numKlasters, dataNum):
+    #If the number of Clusters = [a] (a in N), then we have fixed number
+    #clusters.
+    #If the number of clusters = [a,b] (a,b in N), then we have clusters 
+    #in range[a,b]
+    def generateGenome(self, numberClusters, dataNum):
         l = [i for i in range(dataNum)]
-        random.shuffle(l)        
-        randList =sorted( random.sample(range(dataNum-1),numKlasters-1))
+        random.shuffle(l)
+        
+        if len(numberClusters) == 1:
+            numClusters = numberClusters[0]
+        elif len(numberClusters) == 2:
+            numClusters = random.randint(numberClusters[0],numberClusters[1])
+        else:
+            print "Error numerClusters ([a] or [a,b])"
+            numberClusters = list(input("numberClusters: "))
+        
+        randList =sorted( random.sample(range(dataNum-1),numClusters-1))
         randList.append(dataNum)
         befor = 0
         k = 0
@@ -29,11 +45,7 @@ class Genome:
             self.genome[k] = l[befor:i]
             befor = i
             k+=1
-        
-                        
             
-    
-    
 
 class Population:
     def __init__(self):
