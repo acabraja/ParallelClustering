89d57b87361042bfe1d24673a8610bb93521a60e
Anto Čabraja <cabraja.anto@gmail.com>
2014-06-16 10:18:00 +0200

    khjkj

--- algorithms/kmeans_sec.py
+++ algorithms/kmeans_sec.py
@@ -3,37 +3,44 @@ import  numpy
 import  scipy
 import  pickle
 
-def dist(d1,d2):
-    return 0
+def count(el, cluster):
+    counter = 0.0
+    for c in cluster:
+        for d in c:
+            if el[0] = d[0]:
+                counter += d[1]
+                break
+    return counter
 
-def default_clusters(data, k):
-    return [list(),list()]
+def dist(d1,cluster):
+    distance = 0
+    n = len(cluster)
+    for element in d1:
+        distance += count(element,cluster)/n
+    return distance
 
-def centroid(cluster):
-    return[]
+def default_cntroids(data, k):
+    return random.sample(data, k)
 
-def update_centroids():
-    return []
 
 def k_means(data, iteration, num_clusters):
-    [clusters, centroids] = default_clusters(data, num_clusters)
+    clusters = default_clusters(data, num_clusters)
 
     for i in xrange(iteration):
         new_clusters = []
-        for element in data:
+        for element in data.keys():
             temp = []
-            for c in centroids:
+            for c in clusters:
                 temp.append(dist(c,element))
             new_clusters[temp.index(min(temp))].append(element)
-        centroids = update_centroids()
+        # centroids = update_centroids()
         clusters = new_clusters
     return [clusters, centroids]
 
 def main():
-    # f = open("data.txt", 'rb')
-    # data = pickle.load(f)
-    data = []
-    [clusters, centroids] = k_means(data = data,iteration = 10, num_clusters = 5)
+    f = open("doc.txt", 'rb')
+    data = pickle.load(f)
+    [clusters, centroids] = k_means(data = data,iteration = 10, num_clusters = 3)
 
 if __name__ == '__main__':
     main()
